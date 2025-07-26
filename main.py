4e0a4e64cf7bf5e086de75ccc80b07c13eb86c37
Gunthee <gunthee.kanj@bumail.net>
2025-07-26 07:15:10 +0000

    first commit

--- main.py
+++ main.py
@@ -12,9 +12,24 @@ todos: List[str] = []
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "todos": todos})
+    return templates.TemplateResponse("index.html", {"request": request, "todos": todos, "users": users})
 
 @app.post("/create-todo")
 def create_todo(item: str = Form(...)):
     todos.append(item)
     return RedirectResponse("/", status_code=303)
+
+# Temporary storage for todos (in-memory)
+users: List[str] = []
+@app.post('/create-user')
+def create_todo_user(user: str= Form(...)):
+    users.append(user)
+    return RedirectResponse("/", status_code=303)
+
+# @app.post('/user-post')
+# def user_post():
+    
+
+# @app.get('get-todo')
+# def get_todo():
+#     return
